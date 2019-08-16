b130e82cf4386d5a370dd0d1da64575f86462608
Karthik <karthik@email.com>
2019-08-16 18:39:51 -0400

    Initial Commit

---
+++ src/main/java/com/demo/data/rest/SpringDataRestApplication.java
@@ -0,0 +1,35 @@
+package com.demo.data.rest;
+
+import java.math.BigDecimal;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import com.demo.entity.Employee;
+import com.demo.repositories.EmployeeRepository;
+
+@SpringBootApplication
+@EnableJpaRepositories(basePackages = { "com.demo.repositories" })
+@EntityScan(basePackages = { "com.demo.entity" })
+public class SpringDataRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataRestApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner startup(EmployeeRepository employeeRepository) {
+
+		return (args) -> {
+			System.out.println("=============== SPRING DATA REST =================");
+
+			employeeRepository.save(new Employee("KARTHIK", "CANADA", new BigDecimal("100")));
+		};
+
+	}
+
+}
---
+++ src/main/java/com/demo/entity/Employee.java
@@ -0,0 +1,65 @@
+package com.demo.entity;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Employee {
+
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	private String name;
+
+	private String country;
+
+	private BigDecimal salary;
+
+	public Employee() {
+		super();
+	}
+
+	public Employee(String name, String country, BigDecimal salary) {
+		super();
+		this.name = name;
+		this.country = country;
+		this.salary = salary;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public BigDecimal getSalary() {
+		return salary;
+	}
+
+	public void setSalary(BigDecimal salary) {
+		this.salary = salary;
+	}
+
+}
