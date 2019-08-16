3d9d10364bed0db1b48912165cb81536745b9030
Karthik <karthik@email.com>
2019-08-16 19:25:08 -0400

    Added repository classes

--- src/main/java/com/demo/repositories/EmployeeRepository.java
+++ src/main/java/com/demo/repositories/EmployeeRepository.java
@@ -1,11 +1,33 @@
 package com.demo.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.math.BigDecimal;
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import com.demo.entity.Employee;
 
+/**
+ * 
+ * Rest API can be accessed using the below URL http://localhost:8000/employees
+ * http://localhost:8000/employees/search/findEmployeeById?id=1
+ * 
+ * 
+ * @author Karthik
+ *
+ */
 @RepositoryRestResource
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-	
+public interface EmployeeRepository extends CrudRepository<Employee, Long> {
+
+	@RestResource(path = "findById")
+	public Employee findEmployeeById(@Param("id") Long id);
+
+	public Employee findEmployeeByName(@Param("name") String name);
+
+	@RestResource(path = "findBySalaryAbove")
+	public List<Employee> findEmployeeBySalaryGreaterThan(@Param("salary") BigDecimal salary);
+
 }
